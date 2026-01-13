b4ddadc8917cef6b6ff409672c8b1d2fba405011
Ashish kumar <ashishkumar@Ashishs-MacBook-Air.local>
2026-01-13 22:32:19 +0530

    new init

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { ScrollTrigger,SplitText } from "gsap/all";
+import Navbar from "./components/Navbar";
+import Hero from "./components/hero";
+import gsap from "gsap";
+import Cocktails from "./components/Cocktails"
+gsap.registerPlugin(ScrollTrigger,SplitText);
+
+const App = () => {
+  return (
+  <main>
+    <Navbar />
+    <Hero/>
+<Cocktails/>
+  </main>
+  )
+}
+
+export default App
---
+++ src/components/Cocktails.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const cocktails = () => {
+  return (
+    <div>
+      hi
+    </div>
+  )
+}
+
+export default cocktails
